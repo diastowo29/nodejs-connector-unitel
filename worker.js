e17b2a6eaa881af0b722b74ce6c36505fdfc6738
Diastowo <diastowo@gmail.com>
2023-09-14 14:52:30 +0700

    change respnse

--- worker.js
+++ worker.js
@@ -46,7 +46,7 @@ async function processMessage (jobData, done) {
         let pushPayload = service.pushConversationPayload(ZD_PUSH_API, authToken, jobData.body.instance_id, external_resource_array)
         console.log(JSON.stringify(pushPayload))
         axios(pushPayload).then((response) => {
-            done(null, response);
+            done(null, {response});
         }, (error) => {
             done(new Error(error));
         });
