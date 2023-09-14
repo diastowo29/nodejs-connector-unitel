aeb97ab448f177c86a55c4c538d5bf85990dd789
Diastowo <diastowo@gmail.com>
2023-09-14 17:36:30 +0700

    test

--- worker.js
+++ worker.js
@@ -13,14 +13,14 @@ const EXT_CHAT_TOKEN = process.env.EXT_CHAT_TOKEN || 'xxx';
 let maxJob = 5;
 let workers = maxJob;
 let maxJobsPerWorker = maxJob;
-// let workQueue = require('./config/redis.config')
-let Queue = require('bull');
-let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
-let workQueue = new Queue('sendMessage', REDIS_URL, {
-    settings: {
-        maxStalledCount: 0
-    }
-});
+let workQueue = require('./config/redis.config')
+// let Queue = require('bull');
+// let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+// let workQueue = new Queue('sendMessage', REDIS_URL, {
+//     settings: {
+//         maxStalledCount: 0
+//     }
+// });
 
 function start() {
     workQueue.process(maxJobsPerWorker, async (job, done) => {
