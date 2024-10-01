d66c26197187bac0528ad73cbe1f6e53a6454b91
Diastowo <diastowo@gmail.com>
2024-10-01 09:43:17 +0700

    test

--- config/redis.config.js
+++ config/redis.config.js
@@ -3,14 +3,10 @@ let vars = require('./vars')
 let REDIS_URL = vars.REDIS_URL;
 let workQueue = new Queue('newSendMessage', {
     redis: {
-        host: REDIS_URL.hostname,
-        port: Number(REDIS_URL.port),
-        password: REDIS_URL.password,
-        tls: {
-            rejectUnauthorized: false,
-            requestCert: true,
-            // agent: false, (not all clients accept this)
-        },
+        password: process.env.REDIS_URL.split('@')[0].split(':')[2],
+        host: process.env.REDIS_URL.split('@')[1].split(':')[0],
+        port: parseInt(process.env.REDIS_URL.split('@')[1].split(':')[1]),
+        tls: { rejectUnauthorized: false },
     }
 }, {
     settings: {
