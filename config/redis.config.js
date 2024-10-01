d990d865a65b9e034bcdb106a452a5c2f844b70e
Diastowo <diastowo@gmail.com>
2024-10-01 09:24:10 +0700

    newSendMessage

--- config/redis.config.js
+++ config/redis.config.js
@@ -1,7 +1,7 @@
 let Queue = require('bull');
 let vars = require('./vars')
 let REDIS_URL = vars.REDIS_URL;
-let workQueue = new Queue('sendMessage', REDIS_URL, {
+let workQueue = new Queue('newSendMessage', REDIS_URL, {
     settings: {
         maxStalledCount: 2
     }
