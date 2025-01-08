cdb76e9725d3ce3fe68609b1a2a27d433ab8f1c6
Diastowo <diastowo@gmail.com>
2025-01-08 09:58:36 +0700

    fixing vars

--- config/vars.js
+++ config/vars.js
@@ -1,7 +1,7 @@
 module.exports = {
-    TFIELDS_ID: process.env.USER_TICKET_ID || '6681549599887',
+    TFIELDS_ID: process.env.USER_TICKET_ID || '6355935349903',
     TRANSITION_FIELDS_ID: process.env.TRANSITION_TFIELDS_ID || '11658021114639',
-    ZD_HOST: process.env.ZD_HOST || 'https://unitelgroup1694589998.zendesk.com',
+    ZD_HOST: process.env.ZD_HOST || 'https://unitelgroup.zendesk.com',
     EXT_HOST: process.env.EXT_CHAT_HOST || 'xxx',
     EXT_TOKEN: process.env.EXT_CHAT_TOKEN || 'xxx',
     LOGGLY_TOKEN: process.env.LOGGLY_TOKEN || '25cbd41e-e0a1-4289-babf-762a2e6967b6',
